2ed78a2c6332d7fdf731ece9fa415126b812da07
megankaly5 <megankaly5@gmail.com>
2025-03-20 17:41:41 +0000

    Add 14.ts

---
+++ src/14.ts
@@ -0,0 +1,13 @@
+function randomCode() {
+  // Randomly generate some basic code
+
+  const basicCode = `
+    function sayHello(name: string) {
+      console.log(`Hello, ${name}!`);
+    }
+
+    sayHello("World");
+  `;
+
+  return basicCode;
+}
